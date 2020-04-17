2f33554a6cf9298e75199e5f3d10c5a773bc6ee0
Shasto <k.waligora37@gmail.com>
2020-04-17 19:54:15 +0200

    Add following camera

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCamera : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+    public float maxY;
+
+    Vector3 offset;
+    float lowY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = target.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+
+        if (transform.position.y < maxY) transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
+    }
+}
