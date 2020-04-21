896a60449daae869ec75d4719f48a2ee31416c89
Shasto <k.waligora37@gmail.com>
2020-04-21 12:20:58 +0200

    Add playerStat

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    //health
+    public int maxHealth;
+    int currentHealth;
+
+    //other
+    Animator myAnim;
+    PlayerController playerController;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        myAnim = GetComponent<Animator>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    void TakeDamage(int dmg)
+    {
+        currentHealth -= dmg;
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    void Die()
+    {
+        myAnim.SetTrigger("Death");
+        playerController.enabled = false;
+    }
+}
