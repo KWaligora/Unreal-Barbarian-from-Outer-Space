09346636a64e25b14a881e269bdc4eecfd97674b
Shasto <k.waligora37@gmail.com>
2020-05-20 18:23:11 +0200

    Add animation and code to bowman prefab

--- Assets/Scripts/BowMan.cs
+++ Assets/Scripts/BowMan.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Skeleton : MonoBehaviour, IEnemy
+public class BowMan : MonoBehaviour, IEnemy
 {
     //move
     public Transform pos1, pos2;
@@ -12,8 +12,6 @@ public class Skeleton : MonoBehaviour, IEnemy
     bool canflip = true;
 
     //attack
-    public Transform attackPoint;
-    public float attackRange = 1.0f;
     public int damage;
     public float attackRatio;
     bool canAttack = true;
@@ -37,14 +35,14 @@ public class Skeleton : MonoBehaviour, IEnemy
         myRb = GetComponent<Rigidbody2D>();
         maxSpeed *= -1;
         currentSpeed = maxSpeed;
-        
+
     }
 
     void Update()
     {
         myRb.velocity = new Vector2(currentSpeed, myRb.velocity.y);
         //movement       
-        myAnim.SetFloat("speed", Mathf.Abs(myRb.velocity.x));
+        myAnim.SetFloat("Speed", Mathf.Abs(myRb.velocity.x));
 
         if (transform.position.x <= pos1.position.x && canflip)
             flip();
@@ -52,7 +50,7 @@ public class Skeleton : MonoBehaviour, IEnemy
         else if (transform.position.x >= pos2.position.x && canflip)
             flip();
 
-        canflip = CheckFlip();       
+        canflip = CheckFlip();
     }
 
 
@@ -87,14 +85,14 @@ public class Skeleton : MonoBehaviour, IEnemy
     void Die()
     {
         Rigidbody2D myRB = GetComponent<Rigidbody2D>();
-        GetComponent<CapsuleCollider2D>().enabled = false;       
+        GetComponent<CapsuleCollider2D>().enabled = false;
         Destroy(myRB);
-        
+       
         this.gameObject.layer = 0;
         myAnim.SetTrigger("Die");
         material.SetColor("_Color1", new Color(1, 1, 1, 1));
 
-        ExpBall ball = Instantiate(expBall, GetComponentInParent<Skeleton>().transform.position, Quaternion.Euler(new Vector3(0, 0, 0))).gameObject.GetComponent<ExpBall>();
+        ExpBall ball = Instantiate(expBall, GetComponentInParent<BowMan>().transform.position, Quaternion.Euler(new Vector3(0, 0, 0))).gameObject.GetComponent<ExpBall>();
         ball.Init(expValue);
 
         Destroy(this);
@@ -102,17 +100,7 @@ public class Skeleton : MonoBehaviour, IEnemy
 
     void Attack()
     {
-        myAnim.SetTrigger("Attack");
-        Collider2D [] collider = Physics2D.OverlapCircleAll(attackPoint.position, attackRange);
-        foreach(Collider2D player in collider)
-        {
-            if(player.tag == "Player")
-            {
-                StartCoroutine(SendDamage(player.gameObject.GetComponent<PlayerStats>()));
-                break;
-            }
-        }
-
+        myAnim.SetTrigger("Attack");        
     }
 
     IEnumerator AttackDelay(float delay)
@@ -124,7 +112,7 @@ public class Skeleton : MonoBehaviour, IEnemy
 
     IEnumerator SendDamage(PlayerStats player)
     {
-        yield return new WaitForSeconds(0.5f);        
+        yield return new WaitForSeconds(0.5f);
         player.TakeDamage(damage, transform);
     }
 
@@ -145,8 +133,8 @@ public class Skeleton : MonoBehaviour, IEnemy
 
             StartCoroutine(AttackDelay(attackRatio));
             Attack();
-            currentSpeed = 0;            
-        }            
+            currentSpeed = 0;
+        }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
