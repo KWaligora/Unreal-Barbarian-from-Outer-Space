584945d49a97bfdb44549f1b05bf320a4a32f78d
Shasto <k.waligora37@gmail.com>
2020-05-23 19:14:29 +0200

    Add waitForPlayer bool to mobile platform

--- Assets/Scripts/Other/MobilePlatform.cs
+++ Assets/Scripts/Other/MobilePlatform.cs
@@ -6,6 +6,8 @@ public class MobilePlatform : MonoBehaviour
 {
     public float speed;
     public Transform pos1, pos2;
+    public bool waitForPlayer;
+    bool hasPlayer;
 
     Vector3 nextPos;
 
@@ -21,9 +23,10 @@ public class MobilePlatform : MonoBehaviour
             nextPos = pos2.position;        
 
         if (transform.position == pos2.position)        
-            nextPos = pos1.position;        
+            nextPos = pos1.position;
 
-        transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+        if (waitForPlayer && hasPlayer)
+            transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
     }
 
     void OnDrawGizmos()
@@ -34,12 +37,18 @@ public class MobilePlatform : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("Player"))
+        {
             collision.collider.transform.SetParent(transform);
+            hasPlayer = true;
+        }
     }
 
     void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.tag.Equals("Player"))
+        {
             collision.collider.transform.SetParent(null);
+            hasPlayer = false;
+        }
     }
 }
