2dc7c0ffb3c2a4134d5eb22c6e2885665266eaf9
Shasto <k.waligora37@gmail.com>
2020-05-27 17:18:20 +0200

    Add bossManager

---
+++ Assets/Scripts/Enemys/BossManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossManager : MonoBehaviour
+{
+    public PlayerController playerController;
+    public PlayerStats PlayerStats;
+    public Animator playerAnim;
+    public MageBoss mageboss;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAnim.SetBool("Grounded", true);
+        playerController.enabled = false;
+        PlayerStats.enabled = false;
+        mageboss.enabled = false;
+    }
+
+    public void Run()
+    {
+        playerController.enabled = true;
+        PlayerStats.enabled = true;
+        mageboss.enabled = true;
+    }
+}
