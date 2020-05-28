82629b4f0faebc43883dae159bce4697618881fd
Shasto <k.waligora37@gmail.com>
2020-05-28 13:02:02 +0200

    Finish MainMenu

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -8,6 +8,10 @@ public class MainMenu : MonoBehaviour
 {
 
     public Button startButton;
+    public Button backButton;
+
+    public GameObject menu;
+    public GameObject controlMenu;
 
     void Start()
     {
@@ -18,4 +22,25 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void Control()
+    {
+        menu.active = false;
+        controlMenu.active = true;
+        backButton.Select();
+    }
+
+    public void Back()
+    {
+        controlMenu.active = false;
+        menu.active = true;
+        startButton.Select();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+ 
 }
