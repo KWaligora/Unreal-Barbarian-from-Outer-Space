d0ecb4ed0d3cd0b8ccab59b0abe95877fb0ec092
Shasto <k.waligora37@gmail.com>
2020-05-28 12:31:54 +0200

    Start button is selected by default

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public Button startButton;
+
+    void Start()
+    {
+        startButton.Select();    
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
